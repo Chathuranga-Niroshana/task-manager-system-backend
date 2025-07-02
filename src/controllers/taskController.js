a43fb0756246bab888a135662de4436d21f8d00f
Chathuranga Niroshana <134888303+Chathuranga-Niroshana@users.noreply.github.com>
2025-07-02 22:41:41 +0530

    feat: enhance task management API with CRUD operations and error handling

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -12,3 +12,56 @@ export const createTask = async (req, res, next) => {
         next(error)
     }
 }
+
+export const getTaskById = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const task = await Task.findById(id)
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" })
+        }
+        res.status(200).json(task)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getAllTasks = async (req, res, next) => {
+    try {
+        const tasks = await Task.find()
+        if (!tasks) {
+            return res.status(404).json({ message: "Tasks not available" })
+        }
+        res.status(200).json(tasks)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateTaskById = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const data = req.body
+
+        const updatedTask = await Task.findByIdAndUpdate(id, data, { new: true, runValidators: true })
+        if (!updatedTask) {
+            return res.status(404).json({ message: "Task not found" })
+        }
+        res.status(200).json(updatedTask)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteTaskById = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const deletedTask = await Task.findByIdAndDelete(id)
+        if (!deletedTask) {
+            return res.status(404).json({ message: "Task not found" })
+        }
+        res.status(200).json({ message: "Task deleted" })
+    } catch (error) {
+        next(error)
+    }
+}
